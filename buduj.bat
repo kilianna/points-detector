25bda9a5188902487c4048831990b2a9f6b44804
kildom <kildom@users.noreply.github.com>
2022-08-10 09:14:49 +0200

    Build JAR file

--- buduj.bat
+++ buduj.bat
@@ -1,1 +1,14 @@
-javac -cp C:\Programy\Fiji.app\jars\ij-1.53q.jar Plugin_Wykrywania.java
+@echo off
+
+set JDK_PATH=C:\Programy\Fiji.app\jdk\bin
+set FIJI_PATH=C:\Programy\Fiji.app
+
+if not exist %JDK_PATH%\javac.exe echo Cannot find Java compiler. Install JDK 8 and provide path to its 'bin' directory in the '%0' file.
+if not exist %FIJI_PATH%\jars\ij-1.*.jar echo Cannot find ImageJ jar file. Install Fiji and provide path to it in the '%0' file.
+cd /d %~dp0
+mkdir build 2> NUL > NUL
+mkdir dist 2> NUL > NUL
+%JDK_PATH%\javac -encoding utf8 -cp %FIJI_PATH%\jars\ij-1.*.jar -d build Plugin_Wykrywania.java
+cd build
+%JDK_PATH%\jar cvf ..\dist\Plugin_Wykrywania.jar ..\plugins.config Plugin_Wykrywania*.class
+cd ..
--- buduj.sh
+++ buduj.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 set -e
 
-javac -cp /home/doki/my/Fiji.app/jars/ij-1.*.jar Plugin_Wykrywania.java
-if [ "$1" = "run" ]; then
-	cp -f Plugin_Wykrywania*.class /home/doki/my/Fiji.app/plugins/
+JDK_PATH=
+FIJI_PATH=
+
+if [ $# -gt 1 ]; then
+    FIJI_PATH=$1
+fi
+if [ $# -gt 2 ]; then
+    JDK_PATH=$2
+fi
+
+mkdir -p build
+mkdir -p dist
+$(JDK_PATH)javac -encoding utf8 -cp $(FIJI_PATH)jars/ij-1.*.jar -d build Plugin_Wykrywania.java
+cd build
+$(JDK_PATH)jar cvf ../dist/Plugin_Wykrywania.jar ../plugins.config Plugin_Wykrywania*.class
+cd ..
+
+if [ "$3" = "run" ]; then
+	cp -f dist/Plugin_Wykrywania.jar $(FIJI_PATH)plugins/
 	kill $(xprop -name "(Fiji Is Just) ImageJ" _NET_WM_PID) || echo "OK"
 	sleep 1
-	gnome-terminal --title=FijiTerm -- /home/doki/my/Fiji.app/ImageJ-linux64 /home/doki/my/filtry-ani/test.tif &
+	gnome-terminal --title=FijiTerm -- $(FIJI_PATH)ImageJ-linux64 test.tif &
 	sleep 1
 fi
