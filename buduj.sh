4d99b3d9e9fa20a8f61df4c6d9d5355379027d49
kildom <kildom@users.noreply.github.com>
2023-04-05 18:23:51 +0200

    Add color balance reset plugin

--- buduj.sh
+++ buduj.sh
@@ -13,7 +13,7 @@ fi
 
 mkdir -p build
 mkdir -p dist
-${JDK_PATH}javac -encoding utf8 -cp ${FIJI_PATH}jars/ij-1.*.jar -d build Points_Detector.java Slices_Correction.java Slices_Div.java
+${JDK_PATH}javac -encoding utf8 -cp ${FIJI_PATH}jars/ij-1.*.jar -d build Points_Detector.java Slices_Correction.java Slices_Div.java Colors_Move.java
 cd build
 ${JDK_PATH}jar cvf ../dist/Points_Detector.jar ../plugins.config *.class
 cd ..
@@ -22,6 +22,6 @@ if [ "$3" = "run" ]; then
 	cp -f dist/Points_Detector.jar ${FIJI_PATH}plugins/
 	kill $(xprop -name "(Fiji Is Just) ImageJ" _NET_WM_PID) || echo "OK"
 	sleep 1
-	gnome-terminal --title=FijiTerm -- ${FIJI_PATH}ImageJ-linux64 test.tif &
+	gnome-terminal --title=FijiTerm -- ${FIJI_PATH}ImageJ-linux64 '/media/doki/One Touch/prv/gamma/2022_05_12/napromieniania gamma/500mGy/G_684_2019_500mGy_Cs137_od5um_co0.5um_30s004.nd2' &
 	sleep 1
 fi
