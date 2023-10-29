bbc2723f2dd3dbbdf3734e8a546f97083f0946b8
kildom <kildom@users.noreply.github.com>
2023-10-29 23:33:41 +0100

    Fix folder name for Windows

--- other/BuildNative.sh
+++ other/BuildNative.sh
@@ -10,17 +10,17 @@ echo Compiling native tool with $JDK_INCLUDE
 
 case "$OSTYPE" in
   *darwin*)
-    gcc -c -fPIC $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/darwin -o /tmp/NativeTools.o aux/NativeTools.c
+    gcc -c -fPIC $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/darwin -o /tmp/NativeTools.o other/NativeTools.c
     gcc -dynamiclib -o src/native_tools.dylib /tmp/NativeTools.o -lc
     strip src/native_tools.dylib
     ;;
   *msys* | *win*)
-    gcc -c $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/win32 -o /tmp/NativeTools$ARCH.o aux/NativeTools.c
+    gcc -c $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/win32 -o /tmp/NativeTools$ARCH.o other/NativeTools.c
     gcc -shared -Wl,--kill-at -o src/native_tools$ARCH.dll /tmp/NativeTools$ARCH.o
     strip src/native_tools$ARCH.dll
     ;;
   *)
-    gcc -c -fPIC $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/linux -o /tmp/NativeTools.o aux/NativeTools.c
+    gcc -c -fPIC $FLAGS -I$JDK_INCLUDE -I$JDK_INCLUDE/linux -o /tmp/NativeTools.o other/NativeTools.c
     gcc -shared -fPIC -o src/native_tools.so /tmp/NativeTools.o -lc
     strip src/native_tools.so
 esac
